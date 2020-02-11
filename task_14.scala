ae81f746f18088d549fc8be16559d0c22edae506
Фадеев Алексей <a.fadeev@mt-innovations.ru>
2020-02-11 19:24:16 +0300

    Scala solution

--- task_14.scala
+++ task_14.scala
@@ -1,4 +1,4 @@
-object task_14 extends App{
+object Solution extends App{
 
   // Write a function to find the longest common prefix string amongst an array of strings.
   // If there is no common prefix, return an empty string "".
